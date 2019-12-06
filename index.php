849ee2a87189ac6879cc8d6ee6b19578d2938cb7
Vladyslav Kholodnitskyi <vlad_1210@ukr.net>
2019-12-06 16:34:07 +0200

    form html

--- index.php
+++ index.php
@@ -8,6 +8,20 @@
     <title>Document</title>
 </head>
 <body>
+<!--form-->
+<form action="form.php" method="post">
+    <input name="type" style="display: none" type="text" value="Набор TRY с портативной лампой (<s>985</s>грн) 689грн">
+    <label for="number">Номер телефона *</label><br>
+    <input class="input" id="number" name="number" type="text" value="+ (380) ">
+    <label for="name">Фамилия Имя Отчество *</label><br>
+    <input class="input" id="name" name="name" type="text"><br>
+    <label for="city">Город</label><br>
+    <input class="input" id="city" name="city" type="text"><br>
+    <label for="email">Электронная почта</label><br>
+    <input class="input" id="email" name="email" type="email"><br>
 
+    <input class="button" type="submit" value="КУПИТЬ">
+</form>
+<!--form-->
 </body>
 </html>
