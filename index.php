851ca1fb315205438cf6f3dd1bdae049138ac4da
Vladyslav Kholodnitskyi <vlad_1210@ukr.net>
2019-12-06 19:00:57 +0200

    back img and header fix

--- index.php
+++ index.php
@@ -28,6 +28,9 @@
     <div id="background">
 <!--        background photo-->
     </div>
+    <div style="height: 2000px; width: 100%; background-color: rgba(0,0,0,0.71)">
+
+    </div>
     <!--testform-->
     <form action="form.php" method="post">
         <input name="type" style="display: none" type="text" value="Набор TRY с портативной лампой (<s>985</s>грн) 689грн">
