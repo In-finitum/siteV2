83f8986f5265a55ef12e65bc64c9672b1c409c82
PrimRose <ramraise@gmail.com>
2019-12-08 12:18:35 +0200

    header media

--- index.php
+++ index.php
@@ -27,6 +27,18 @@
                 <li><a href="#">ВІДГУКИ</a></li>
                 <li><a href="#">ПРО НАС</a></li>
             </ul>
+        <div class="dropdown">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Меню
+            </a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">ПОРТМОНЕ</a>
+                <a class="dropdown-item" href="#">БЛОКНОТИ</a>
+                <a class="dropdown-item" href="#">БАНАНКИ</a>
+                <a class="dropdown-item" href="#">ВІДГУКИ</a>
+                <a class="dropdown-item" href="#">ПРО НАС</a>
+            </div>
+        </div>
         <div class="contacts_header">+38(093)9228738</div>
     </nav>
 </header>
